fda00fb646812d86221bb8afaa23866283e656b3
Pablo Román <pablo.roman85@gmail.com>
2023-08-18 11:14:06 +0200

    Basic setup

---
+++ layouts/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app-layout">
+        <GlobalAppHeader/>
+        <main>
+            <div class="min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white sm:flex sm:justify-center sm:items-center p-6">
+                <div class="mx-auto max-w-screen-md relative">
+                    <slot/>
+                </div>
+            </div>
+        </main>
+        <GlobalAppFooter/>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+
+<div class="px-4 text-center sm:px-6 lg:px-8">
+  <div>
+    <h1 class="text-4xl font-bold tracking-tight text-gray-900">__('My App') </h1>
+    <p class="mx-auto mt-4 max-w-xl text-base text-gray-500">__('Esta es mi app, la mejor del mundo, te das cuen?')</p>
+  </div>
+</div>
+
+</template>
+
+<script setup>
+definePageMeta({
+  layout: 'app-layout'
+})
+</script>
