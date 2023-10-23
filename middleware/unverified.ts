f9def205f0cfd93e4ffe0cc58efcb63a15c16d1f
Pablo Román <pablo.roman85@gmail.com>
2023-10-23 20:49:52 +0200

    Remove auth and middleware files

--- middleware/unverified.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtRouteMiddleware(() => {
-    const user = useUser();
-  
-    if (!user.value) return navigateTo("/auth/login");
-  
-    // @ts-ignore
-    if (user.value.email_verified_at || user.value.is_verified)
-      return navigateTo("/");
-  });
