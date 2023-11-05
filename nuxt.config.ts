c70101011303651a64916a8848562a38d67ab43e
Pablo Román <pablo.roman85@gmail.com>
2023-11-05 20:34:48 +0100

    Add page title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
+  ssr: true,
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
     '@vueuse/nuxt'
   ],
+  app:{
+    head: {
+        title: 'The World Cup Archives',
+    },
+  },
   runtimeConfig: {
     public: {
       API_BASE_URL: process.env.API_BASE_URL,
