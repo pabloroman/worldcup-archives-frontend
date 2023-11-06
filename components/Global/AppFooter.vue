4fa97c79d01f20c70b52558a96e9506a3ca42236
Pablo Román <pablo.roman85@gmail.com>
2023-11-06 10:47:35 +0100

    Add colophon

--- components/Global/AppFooter.vue
+++ components/Global/AppFooter.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <div class="max-w-screen-lg mx-auto py-10 px-6 lg:px-0 text-slate-400 text-sm leading-relaxed">
+    <p><strong>The World Cup Archives</strong> is a project by <a href="https://pabloroman.es" class="underline" target="_blank">Pablo Román</a> published under a <a href="https://creativecommons.org/licenses/by-sa/4.0/legalcode" class="underline" target="_blank">CC-BY-SA 4.0 license</a></p>
+    <p>Database sourced from <a href="https://github.com/jfjelstul/worldcup" class="underline">The Fjelstul World Cup Database</a> © 2023 Joshua C. Fjelstul, Ph.D.</p>
+    <p>Source code available in <a href="https://github.com/pabloroman" class="underline" target="_blank">Github</a></p>
+    </div>
 </template>
 
 <script setup>
--- layouts/AppLayout.vue
+++ layouts/AppLayout.vue
@@ -4,6 +4,7 @@
         <main>
             <slot/>
         </main>
+        <AppFooter/>
     </div>
 </template>
 
