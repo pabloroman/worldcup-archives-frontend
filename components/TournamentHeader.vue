4c0de4a6abec5a2b9fde7203cee5f27e6f277917
Pablo Román <pablo.roman85@gmail.com>
2023-11-07 15:56:56 +0100

    Add tournament dates

--- components/TournamentHeader.vue
+++ components/TournamentHeader.vue
@@ -2,7 +2,7 @@
     <div>
         <header class="bg-gradient-to-br from-[#0E1E2C] from-20% via-[#000000] via-30% to-[#283B45]">
             <div class="max-w-screen-lg mx-auto py-10 px-6 lg:px-0">
-                <div class="text-white opacity-75">{{ tournament.name }}</div>
+                <div class="text-white opacity-75">{{ tournament.name }} &bull; {{ tournamentStartDate }} - {{ tournamentEndDate }}</div>
                 <div class="flex items-center space-x-4 mt-2">
                 <HostFlag :tournament="tournament"></HostFlag>
                 <h1 class="text-4xl lg:text-5xl font-bold uppercase text-white shadow-md">{{ tournament.host_country }} {{ tournament.year }}</h1>
@@ -21,5 +21,13 @@
             type: Object,
             required: true,
         }
+    });
+
+    const tournamentStartDate = computed(() => {
+        return new Date(props.tournament.start_date).toLocaleDateString('en-US', { day: 'numeric', month: 'long' });
+    });
+
+    const tournamentEndDate = computed(() => {
+        return new Date(props.tournament.end_date).toLocaleDateString('en-US', { day: 'numeric', month: 'long' });
     });
 </script>
