a20ebd73929997f4a6f7284e8eca3ec0996fce95
Pablo Román <pablo.roman85@gmail.com>
2023-11-07 23:15:48 +0100

    Fix display of yellow/red cards

--- components/GamePlayerAppearance.vue
+++ components/GamePlayerAppearance.vue
@@ -3,16 +3,16 @@
     <div class="space-x-2 lg:text-lg">
         <span class="font-bold">{{ player.number }}</span><span>{{ player.player }}</span><span class="text-sm text-slate-400" >{{ player.position }}</span>
     </div>
-    <div v-for="booking in bookings.filter(booking => booking.home_team == home_team && player.player == booking.name)">
-        <div v-if="booking.yellow_card">
+    <div v-for="booking in bookings.filter(booking => booking.home_team == home_team && player.player == booking.name)" class="flex items-center">
+        <template v-if="booking.second_yellow_card">
+            <div class="inline-block w-2 h-3 bg-yellow-400 rounded-sm"></div>&nbsp;<div class="inline-block w-2 h-3 bg-yellow-400 rounded-sm"></div>&nbsp;<span class="text-sm">{{ booking.minute }}</span>
+        </template>
+        <template v-else-if="booking.yellow_card">
             <div class="inline-block w-2 h-3 bg-yellow-400 rounded-sm"></div>&nbsp;<span class="text-sm">{{ booking.minute }}</span>
-        </div>
-        <div v-if="booking.red_card">
+        </template>
+        <template v-else-if="booking.red_card">
             <div class="inline-block w-2 h-3 bg-red-600 rounded-sm"></div>&nbsp;<span class="text-sm">{{ booking.minute }}</span>
-        </div>
-        <div v-if="booking.second_yellow_card">
-            <div class="inline-block w-2 h-3 bg-yellow-400 rounded-sm"></div>&nbsp;<span class="text-sm">{{ booking.minute }}</span>
-        </div>
+        </template>
     </div>
     </div>
 </template>
